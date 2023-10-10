3e9acd907b90bc8042ec27bcc7504dd0b03e1438
Imhyung Lee <rhdtn987@gmail.com>
2023-10-10 15:15:23 +0900

    capstone1

--- backend/v_env/Scripts/activate.bat
+++ backend/v_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Windows\System32\project\backend\v_env
+set VIRTUAL_ENV=C:\Users\SAMSUNG\Desktop\임형대학\capstone\project\backend\v_env
 
 if not defined PROMPT set PROMPT=$P$G
 
